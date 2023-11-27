fda28ead1bf42d1cbee471680d99e2c82c951a56
Giovanni <75858528+srprompt@users.noreply.github.com>
2023-11-27 10:10:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use((req,res,next) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: `https://seahorse-app-64ieu.ondigitalocean.app/`,
   })
 );
 app.use(cookieParser());
